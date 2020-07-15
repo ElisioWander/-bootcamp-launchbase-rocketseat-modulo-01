29d1729471f0baf17fed7c0e7e1fa15780c9a2ba
Elisio Wander <elisio741@hotmail.com>
2020-07-14 22:23:52 -0300

    launchbase-rocketseat

--- iniciando-no-frontend/desafios/03/pages/conteudos/script.js
+++ iniciando-no-frontend/desafios/03/pages/conteudos/script.js
@@ -1,14 +1,12 @@
 const cards = document.querySelectorAll('.card')
 const modalOverlay = document.querySelector('.modal-overlay')
 
-
 for (let card of cards) {
     card.addEventListener('click', function() {
         const courseId = card.getAttribute('id')
         
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${courseId}` 
-
     })
 }
 
@@ -16,3 +14,4 @@ modalOverlay.querySelector('.close-modal').addEventListener('click', function() 
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector('iframe').src = ''
 })
+
