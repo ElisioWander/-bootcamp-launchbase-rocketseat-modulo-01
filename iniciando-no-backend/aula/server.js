926789f4ec304d34a9fde9e63ef7ab8544b6b3b8
Elisio Wander <elisio741@hotmail.com>
2020-07-29 01:10:55 -0300

    deletando

--- iniciando-no-backend/aula/server.js
+++ iniciando-no-backend/aula/server.js
@@ -1,10 +1,24 @@
 const express = require("express")
+const nunjucks = require("nunjucks")
+
 const server = express()
 
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.use(express.static("public"))
+
 server.get("/", (req, res) => {
-    return res.send("Hello!")
+    return res.render("about")
+})
+
+server.get("/portifolio", (req, res) => {
+    return res.render("portifolio")
 })
 
 server.listen(5000, function() {
-    console.log("Server is runing")
+
 })
