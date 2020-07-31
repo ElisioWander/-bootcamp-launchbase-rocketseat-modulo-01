fd5f4963df1bb9b6225b00b6796334c15f15ea91
Elisio Wander <elisio741@hotmail.com>
2020-07-30 23:38:48 -0300

    backend-launchbase

--- iniciando-no-backend/desafios-03/part01/server.js
+++ iniciando-no-backend/desafios-03/part01/server.js
@@ -11,8 +11,17 @@ nunjucks.configure("views", {
     express: server
 })
 
+
 server.get("/", (req, res) => {
-    return res.render("index")
+    return res.render("portifolio")
+})
+
+server.get("/about", (req, res) => {
+    return res.render("about")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("partials/not-found");
 })
 
 server.listen(5000, () => {
