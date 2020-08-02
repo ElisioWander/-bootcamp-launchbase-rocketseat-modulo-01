b51203114020677d5d01e8e789e409f438bbe93b
Elisio Wander <elisio741@hotmail.com>
2020-08-02 20:52:54 -0300

    launchbase-rocketseat

---
+++ iniciando-no-backend/desafios-03/part-03/data.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        id: "starter",
+        number: "01",
+        numberClass: 'class="card-number-one"',
+        image: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+        description: '<span class="purple">Torne-se um programador desejado</span> no mercado com esses cursos gratuitos',
+        modules: "5 Módulos",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        number: "02",
+        numberClass: 'class="card-number-two"',
+        image: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
+        description: '<span class="orange">Domine programação do zero</span> e tenha acesso as melhores oportunidades do mercado',
+        modules: "16 Módulos",
+        price: "Pago"
+    },
+    {
+        id: "gostack",
+        number: "03",
+        numberClass: 'class="card-number-three"',
+        image: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg",
+        description: '<span class="green">Treinamento imercivo</span> nas tecnologias mais modernas de desenvolvimento web e mobile',
+        modules: "18 Módulos",
+        price: "Pago"
+    }
+]
