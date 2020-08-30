415fe83a71459e8b1dd1ca324f22800ce8a69f3f
Elisio Wander <elisio741@hotmail.com>
2020-08-29 23:12:29 -0300

    gym-project

--- gerenciador-de-academia/server.js
+++ gerenciador-de-academia/server.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const nunjucks = require("nunjucks")
-const videos = require("./data")
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
 
 const server = express()
 
@@ -13,40 +13,7 @@ nunjucks.configure("views", {
 })
 
 server.use(express.static("public"))
-
-server.get("/", (req, res) => {
-    const about = {
-        avatar_url: 'https://avatars1.githubusercontent.com/u/27742439?s=460&u=89474e52344ce457de62e25a70512d42042303d9&v=4',
-        name: "Elisio Wander",
-        role: "Estudante de programação",
-        description: 'Iniciando nos estudos de Desenvolvimento Web atravéz do curso louchbase da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>, focado nas seguintes tecnologias: HTML, CSS, JavaScript e Node.js.',
-        link: [
-            {name: "github", url: "https://github.com/ElisioWander"},
-            {name: "twitter", url: "https://twitter.com/Elisio741"},
-            {name: "linkedin", url: "https://www.linkedin.com/in/elisio-wander-b88b69136/"}
-        ]
-    }
-
-    return res.render("about", { about })
-})
-
-server.get("/portifolio", (req, res) => {
-    return res.render("portifolio", { items: videos })
-})
-
-server.get("/video", (req, res) => {
-    const id = req.query.id
-
-    const video = videos.find(function(video) {
-        return video.id == id
-    })
-
-    if (!video) {
-        res.send("Video not found")
-    }
-
-    return res.render("video", { item: video })
-})
+server.use(routes)
 
 server.listen(5000, function() {
 
