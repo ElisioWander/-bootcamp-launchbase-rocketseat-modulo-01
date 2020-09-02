59d3e054f82eee0a962fe4db3b475236766aaa54
Elisio Wander <elisio741@hotmail.com>
2020-09-01 22:31:39 -0300

    gym-project

---
+++ gerenciador-de-academia/gym-project/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+
+const server = express()
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.use(express.static("public"))
+server.use(routes)
+
+server.listen(5000, function() {
+
+})
