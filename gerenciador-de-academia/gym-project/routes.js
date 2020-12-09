d754c1f1391dd123fc354d7958b3fef630a36ac1
Elisio Wander <elisio741@hotmail.com>
2020-12-09 00:06:29 -0300

    gym-project

--- gerenciador-de-academia/gym-project/routes.js
+++ gerenciador-de-academia/gym-project/routes.js
@@ -19,6 +19,10 @@ routes.get("/instructors/create", (req, res) => {
 
 routes.get("/instructors/:id", instructors.show)
 
+routes.get("/instructors/:id/edit", function(req, res) {
+    return res.render("instructors/edit.html")
+})
+
 
 /*POST ROUTES*/
 routes.post("/instructors", instructors.post)
