d326027fe6dc46b3b087d471470ba58fcb5bd4d4
Elisio Wander <elisio741@hotmail.com>
2021-01-13 23:32:52 -0300

    launchbase-challange

---
+++ gerenciador-de-academia/desafios/part-06/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const teachers = require('./teachers')
+const routes = express.Router()
+
+routes.get("/", (req, res) => {
+    return res.redirect("/teachers")
+})
+
+
+routes.get("/teachers", teachers.index)
+routes.get("/teachers/create", teachers.create)
+routes.get("/teachers/:id", teachers.show)
+routes.get("/teachers/:id/edit", teachers.edit)
+routes.post("/teachers", teachers.post)
+routes.put("/teachers", teachers.update)
+routes.delete("/teachers", teachers.delete)
+
+
+
+
+
+
+module.exports = routes
