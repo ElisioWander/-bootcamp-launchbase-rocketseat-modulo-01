d686cec1ef696711e079be2428f73c0a09a765f9
Elisio Wander <elisio741@hotmail.com>
2021-02-04 23:14:24 -0300

    gym-manager

--- gerenciador-de-academia/gym-project-with-database/public/script.js
+++ gerenciador-de-academia/gym-project-with-database/public/script.js
@@ -7,3 +7,20 @@ for(item of menuItems) {
         item.classList.add('active')
     }
 }
+
+//totalPages = 20
+//selectedPage = 15
+//[1...13, 14, 15, 16, 17...20]
+
+let totalPages = 20,
+    selectedPage = 1,
+    pages = []
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    pages.push(currentPage)
+}
+
+console.log(pages)
+
+
+
