66784eaa859e8ea1b3085165aa12f0179f985127
Elisio Wander <elisio741@hotmail.com>
2021-02-17 23:07:10 -0300

    launchstore-pj

--- launchstore/launchstore-project/src/routes.js
+++ launchstore/launchstore-project/src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
-
 const routes = express.Router()
+const ProductsController = require('./app/controllers/ProductsController')
 
 /*GET ROUTES*/
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
     return res.render("layout.html")
+})
+routes.get('/products/create', ProductsController.create)
+routes.get('ads/create', function(req, res) {
+    return res.redirect("/products/create")
 })
 
 module.exports = routes
