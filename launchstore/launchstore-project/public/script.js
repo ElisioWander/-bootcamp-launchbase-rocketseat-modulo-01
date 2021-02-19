f399fd281845697bc676742549e96ebccd305536
Elisio Wander <elisio741@hotmail.com>
2021-02-19 01:00:28 -0300

    launchstore-pj-currency-function

--- launchstore/launchstore-project/public/script.js
+++ launchstore/launchstore-project/public/script.js
@@ -1,23 +1,40 @@
-const input = document.querySelector('input[name="price"]')
-input.addEventListener('keydown', function(event) {
+// const input = document.querySelector('input[name="price"]')
+// input.addEventListener('keydown', function(event) {
     
-    setTimeout(function() {
-        let { value } = event.target
+//     setTimeout(function() {
+//         let { value } = event.target
 
-        //first time passing through "replace and NumberFormat"
-        //1 will transform in -> R$1,00 real
-        //second time
-        //R$1,00 will transform in -> 100
-        //to fix the problem you've gotta do this: R$1,00 -> 100 / 100
-        //this will return number 1 again
-        //another exemple: R$180,23 -> 18023 / 100 will return 180.23
+//         //first time passing through "replace and NumberFormat"
+//         //1 will transform in -> R$1,00 real
+//         //second time
+//         //R$1,00 will transform in -> 100
+//         //to fix the problem you've gotta do this: R$1,00 -> 100 / 100
+//         //this will return number 1 again
+//         //another exemple: R$180,23 -> 18023 / 100 will return 180.23
+//         value = value.replace(/\D/g, "")
+
+//         value = Intl.NumberFormat('pt-BR', {
+//             style: 'currency',
+//             currency: 'BRL'
+//         }).format(value/100)
+
+//         event.target.value = value
+//     }, 1)
+// })
+
+
+const Mask = {
+    apply(input, func) {
+        setTimeout(function() {
+            input.value = Mask[func](input.value)
+        }, 1)
+    },
+    formatBRL(value) {
         value = value.replace(/\D/g, "")
 
-        value = Intl.NumberFormat('pt-BR', {
+        return Intl.NumberFormat("pt-BR", {
             style: 'currency',
             currency: 'BRL'
         }).format(value/100)
-
-        event.target.value = value
-    }, 1)
-})
+    }
+}
