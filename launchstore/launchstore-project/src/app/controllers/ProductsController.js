bf966185ee1214608f007323a2048eec5c57969b
Elisio Wander <elisio741@hotmail.com>
2021-02-18 23:50:08 -0300

    launchbase-pj-currencyFormat

--- launchstore/launchstore-project/src/app/controllers/ProductsController.js
+++ launchstore/launchstore-project/src/app/controllers/ProductsController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/Category')
+const Product = require('../models/Product')
 
 module.exports = {
     create(req, res) {
@@ -6,14 +7,26 @@ module.exports = {
         .then(function(results) {
             const categories = results.rows
 
-            console.log(categories)
-
             return res.render("products/create.html", { categories })
         }).catch(function(err) {
             throw new Error(err)
         })
     },
-    post(req, res) {
+    async post(req, res) {
+        const keys = Object.keys(req.body)
 
+        for(key of keys) {
+            if(req.body[key] == "") {
+                return res.send("Please, fill all fields!")
+            }
+        }
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render("products/create.html", { productId, categories })
     }
 }
